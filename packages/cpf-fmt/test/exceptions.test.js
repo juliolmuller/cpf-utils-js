2129037b5eaf59234e44bb73da31e48eaba07eaa
Julio Muller <juliodolago@gmail.com>
2021-07-23 00:29:36 -0300

    Switch to Rollup bundler

--- packages/cpf-fmt/test/exceptions.test.js
+++ packages/cpf-fmt/test/exceptions.test.js
@@ -1,38 +1,39 @@
-const cpfFmt = require('../build');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const cpfFmt = require('..')
 
 test('Option with range start -1 throws TypeError', () => {
   const cpf = () => cpfFmt('80976511061', {
     hidden: true,
     hiddenRange: { start: -1 },
-  });
-  expect(cpf).toThrow(TypeError);
-});
+  })
+  expect(cpf).toThrow(TypeError)
+})
 
 test('Option with range start greater than 10 throws TypeError', () => {
   const cpf = () => cpfFmt('80976511061', {
     hidden: true,
     hiddenRange: { start: 11 },
-  });
-  expect(cpf).toThrow(TypeError);
-});
+  })
+  expect(cpf).toThrow(TypeError)
+})
 
 test('Option with range end -1 throws TypeError', () => {
   const cpf = () => cpfFmt('80976511061', {
     hidden: true,
     hiddenRange: { end: -1 },
-  });
-  expect(cpf).toThrow(TypeError);
-});
+  })
+  expect(cpf).toThrow(TypeError)
+})
 
 test('Option with range end greater than 10 throws TypeError', () => {
   const cpf = () => cpfFmt('80976511061', {
     hidden: true,
     hiddenRange: { end: 11 },
-  });
-  expect(cpf).toThrow(TypeError);
-});
+  })
+  expect(cpf).toThrow(TypeError)
+})
 
 test('Option with onfail as not a function throws TypeError', () => {
-  const cpf = () => cpfFmt('80976511061', { onFail: 'testing' });
-  expect(cpf).toThrow(TypeError);
-});
+  const cpf = () => cpfFmt('80976511061', { onFail: 'testing' })
+  expect(cpf).toThrow(TypeError)
+})
