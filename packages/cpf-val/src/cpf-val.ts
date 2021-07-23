3a7454d873ad8736841ff19cbbd9b9f2f3f21701
Julio Muller <juliodolago@gmail.com>
2021-07-23 00:59:57 -0300

    Migrate source code to TypeScript

---
+++ packages/cpf-val/src/cpf-val.ts
@@ -0,0 +1,20 @@
+import cpfGen from '@lacussoft/cpf-gen'
+import numOnly from 'num-only'
+
+/**
+ * Validate a given CPF (Brazilian ID document) char sequence.
+ */
+function cpfVal(cpfString: string) {
+  const CPF_LENGTH = 11
+  const cpfDigits = numOnly(cpfString)
+
+  if (cpfDigits.length !== CPF_LENGTH) {
+    return false
+  }
+
+  return cpfDigits === cpfGen({
+    prefix: cpfDigits.substring(0, 9),
+  })
+}
+
+export default cpfVal
--- packages/cpf-val/src/types.d.ts
+++
@@ -1,4 +0,0 @@
-
-declare const cpfVal: (cpfString: string) => boolean;
-
-export default cpfVal;
