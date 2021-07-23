be8164f583947e70fc7a8b4c2affffc24cfe7906
Julio Muller <juliodolago@gmail.com>
2021-07-23 01:00:08 -0300

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,21 @@
 /* eslint-env node */
-
 module.exports = {
   env: {
-    browser: true,
-    es6: true,
-    jest: true,
+    'shared-node-browser': true,
+    'es6': true,
+    'jest': true,
   },
   extends: [
     'lacussoft',
+    'lacussoft/typescript',
   ],
   ignorePatterns: [
     'build/',
     'dist/',
-    '**/*.d.ts',
     '!.babelrc.js',
   ],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     'no-magic-numbers': ['warn', { ignore: [0, 1, 9, 10, 11] }],
-    'semi': ['error', 'always'],
-    'semi-style': ['error', 'last'],
   },
-};
+}
