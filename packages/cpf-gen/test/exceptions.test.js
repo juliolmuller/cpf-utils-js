1df952911d53dd65368e640951f0feff829afa77
Julio Muller <juliodolago@gmail.com>
2021-07-23 00:51:35 -0300

    Switch to Rollup bundler

--- packages/cpf-gen/test/exceptions.test.js
+++ packages/cpf-gen/test/exceptions.test.js
@@ -1,6 +1,6 @@
-const cpfGen = require('../build');
+const cpfGen = require('..')
 
 test('Prefixed value cannot accept string with more than 9 digits', () => {
-  expect(() => cpfGen({ prefix: '1234567890' })).toThrow(Error);
-  expect(() => cpfGen({ prefix: '123.456.789-0' })).toThrow(Error);
-});
+  expect(() => cpfGen({ prefix: '1234567890' })).toThrow(Error)
+  expect(() => cpfGen({ prefix: '123.456.789-0' })).toThrow(Error)
+})
