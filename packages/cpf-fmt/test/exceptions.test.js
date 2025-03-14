a0b900b0aba0b561a3321b047a9dc101608d6ecf
juliolmuller <juliolmuller@outlook.com>
2025-03-14 01:29:12 -0300

    refactor(fmt): lint source code

--- packages/cpf-fmt/test/exceptions.test.js
+++ packages/cpf-fmt/test/exceptions.test.js
@@ -1,39 +1,55 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const cpfFmt = require('..')
+import { expect, test } from 'bun:test';
+
+import cpfFmt from '..';
 
 test('Option with range start -1 throws TypeError', () => {
-  const cpf = () => cpfFmt('80976511061', {
-    hidden: true,
-    hiddenRange: { start: -1 },
-  })
-  expect(cpf).toThrow(TypeError)
-})
+  function testCase() {
+    return cpfFmt('80976511061', {
+      hidden: true,
+      hiddenRange: { start: -1 },
+    });
+  }
+
+  expect(testCase).toThrow(TypeError);
+});
 
 test('Option with range start greater than 10 throws TypeError', () => {
-  const cpf = () => cpfFmt('80976511061', {
-    hidden: true,
-    hiddenRange: { start: 11 },
-  })
-  expect(cpf).toThrow(TypeError)
-})
+  function testCase() {
+    return cpfFmt('80976511061', {
+      hidden: true,
+      hiddenRange: { start: 11 },
+    });
+  }
+
+  expect(testCase).toThrow(TypeError);
+});
 
 test('Option with range end -1 throws TypeError', () => {
-  const cpf = () => cpfFmt('80976511061', {
-    hidden: true,
-    hiddenRange: { end: -1 },
-  })
-  expect(cpf).toThrow(TypeError)
-})
+  function testCase() {
+    return cpfFmt('80976511061', {
+      hidden: true,
+      hiddenRange: { end: -1 },
+    });
+  }
+
+  expect(testCase).toThrow(TypeError);
+});
 
 test('Option with range end greater than 10 throws TypeError', () => {
-  const cpf = () => cpfFmt('80976511061', {
-    hidden: true,
-    hiddenRange: { end: 11 },
-  })
-  expect(cpf).toThrow(TypeError)
-})
+  function testCase() {
+    return cpfFmt('80976511061', {
+      hidden: true,
+      hiddenRange: { end: 11 },
+    });
+  }
+
+  expect(testCase).toThrow(TypeError);
+});
 
 test('Option with onfail as not a function throws TypeError', () => {
-  const cpf = () => cpfFmt('80976511061', { onFail: 'testing' })
-  expect(cpf).toThrow(TypeError)
-})
+  function testCase() {
+    return cpfFmt('80976511061', { onFail: 'testing' });
+  }
+
+  expect(testCase).toThrow(TypeError);
+});
