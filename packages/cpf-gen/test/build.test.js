d9c210004d278660395267646c5682b222b9518b
juliolmuller <juliolmuller@outlook.com>
2025-04-14 00:34:11 -0300

    test: create tests to check built bundles

---
+++ packages/cpf-gen/test/build.test.js
@@ -0,0 +1,41 @@
+import Bun from 'bun';
+import { describe, expect, test } from 'bun:test';
+
+describe('build in CommonJS', () => {
+  const filePath = Bun.resolveSync('../build/index.cjs', import.meta.dir);
+  const file = Bun.file(filePath);
+
+  test('file exists', async () => {
+    await expect(file.exists()).resolves.toBe(true);
+  });
+
+  test('file contains a "module.exports" assignment', async () => {
+    await expect(file.text()).resolves.toContain('module.exports = cpfGen');
+  });
+});
+
+describe('build in ES Module', () => {
+  const filePath = Bun.resolveSync('../build/index.mjs', import.meta.dir);
+  const file = Bun.file(filePath);
+
+  test('file exists', async () => {
+    await expect(file.exists()).resolves.toBe(true);
+  });
+
+  test('file contains a "export default" expression', async () => {
+    await expect(file.text()).resolves.toContain('export { cpfGen as default }');
+  });
+});
+
+describe('build types', () => {
+  const filePath = Bun.resolveSync('../build/index.d.ts', import.meta.dir);
+  const file = Bun.file(filePath);
+
+  test('file exists', async () => {
+    await expect(file.exists()).resolves.toBe(true);
+  });
+
+  test('file contains a "export default" expression', async () => {
+    await expect(file.text()).resolves.toContain('export { cpfGen as default }');
+  });
+});
